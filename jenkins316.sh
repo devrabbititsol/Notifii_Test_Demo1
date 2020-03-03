c94a332fea2182e5a69ef6d078efad53030a98cf
santhosh-annam <santhosh.annam@devrabbit.com>
2020-03-03 17:14:15 +0530

    Testing in stag env

--- jenkins316.sh
+++
@@ -1,5 +0,0 @@
-source ~/.bash_rc
-cd /home/devrabbit/SmartQE/SmartQE-Users/7/Selenium/test_notifii_demo
-curl -d @config316.xml -H 'Content-Type: text/xml' 'http://devrabbit:rabbit@192.168.1.141:8080/createItem?name=test_notifii_demo' --user devrabbit:114de69b453efd3524fce8756b1ed0e69c
-curl -X POST http://devrabbit:rabbit@192.168.1.141:8080/job/test_notifii_demo/config.xml --user devrabbit:114de69b453efd3524fce8756b1ed0e69c --data-binary @config316.xml
-curl -X POST http://devrabbit:rabbit@192.168.1.141:8080/job/test_notifii_demo/build --user devrabbit:114de69b453efd3524fce8756b1ed0e69c
